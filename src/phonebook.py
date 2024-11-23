09e06a5199562f941809af12bb91046b9cd59470
tainahmr <tainahmr@gmail.com>
2024-11-23 14:54:47 -0300

    Bugs levantados e testes unitários construídos

--- src/phonebook.py
+++ src/phonebook.py
@@ -5,29 +5,19 @@ class Phonebook:
 
     def add(self, name, number):
         """
-        #BUG - o number não foi int e sim string, o que significa que number "ABCDE" irá passar
         :param name: name of person in string
         :param number: number of person in string
         :return: 'Nome invalido' or 'Numero invalido' or 'Numero adicionado'
         """
-        #BUG - nome com outros caracteres especiais serão cadastrados, ex "? / \ | , . ; :, etc"
-        if '#' in name:
-            return 'Nome invalido'
-        if '@' in name:
-            # BUG: Mensagem escrita errada
-            return 'Nme invalido'
-        if '!' in name:
-            return 'Nome invalido'
-        if '$' in name:
-            #BUG: Mensagem escrita errada
-            return 'Nome invalio'
-        if '%' in name:
+
+        if any(char in name for char in ['#', '@', '!', '%', '$']):
             return 'Nome invalido'
 
-        #BUG: Nunca vai entrar nesse if pois uma string vazia tem tamanho 0
-        if len(number) < 0:
+        #BUG: Nunca vai entrar nesse if pois uma string vazia não tem tamanho < 0
+        #CORREÇÃO: substituido < por ==
+        if len(number) == 0:
             # BUG: Mensagem escrita errada
-            return 'Numero invalid'
+            return 'Numero invalido'
 
         if name not in self.entries:
             self.entries[name] = number
@@ -37,37 +27,28 @@ class Phonebook:
     def lookup(self, name):
         """
         :param name: name of person in string
+        BUG - o retorno tem sido apenas com número, deveria retornar número + nome
         :return: return number of person with name
+        MELHORIA: retirado os if´s e deixei um único
         """
-        if '#' in name:
-            # BUG: Mensagem escrita errada
-            return 'Nome invaldo'
-        if '@' in name:
+        if any(char in name for char in ['#', '@', '!', '%', '$']):
             return 'Nome invalido'
-        if '!' in name:
-            # BUG: Mensagem escrita errada
-            return 'Nme invalido'
-        if '$' in name:
-            return 'Nome invalido'
-        if '%' in name:
-            # BUG: Mensagem escrita errada
-            return 'Nome nvalido'
-
-        return self.entries[name]
+        #CORREÇÃO - inserido o name para listar number + name
+        return name, self.entries[name]
 
     def get_names(self):
         """
-
         :return: return all names in phonebook
         """
         return self.entries.keys()
 
     def get_numbers(self):
         """
-
+        BUG: os valores tem retornado como dict
         :return: return all numbers in phonebook
+        BUG CORRIGIDO: transformar retorno dos valores em uma lista
         """
-        return self.entries.values()
+        return list(self.entries.values())
 
     def clear(self):
         """
@@ -79,32 +60,36 @@ class Phonebook:
 
     def search(self, search_name):
         """
+        BUG: Retorna lista contendo todos os nomes EXCETO o que esta sendo procurado, devido o NOT IN
         Search all substring with search_name
         :param search_name: string with name for search
         :return: return list with results of search
+        CORREÇÃO: Retirado o 'not in' do if
         """
-        # BUG: Retorna lista contendo todos os nomes EXCETO o que esta sendo procurado
+
         result = []
         for name, number in self.entries.items():
-            if search_name not in name:
+            if search_name in name:
                 result.append({name, number})
         return result
 
     def get_phonebook_sorted(self):
         """
-
+        BUG: Não vai retornar sorted, pois não foi inserido o sorted()
         :return: return phonebook in sorted order
+        BUG CORRIGIDO: Foi inserido o sorted()
         """
-        # BUG: Nunca vai retornar sorted, pois não foi inserido o .sorted()
-        return self.entries
+
+        return sorted(self.entries)
 
     def get_phonebook_reverse(self):
         """
-
+        BUG: Não vai retornar reverse, pois não foi inserido o .reversed() e os dados estão como dict.
         :return: return phonebook in reverse sorted order
+        BUG CORRIGIDO: Inserir o reverse e transformar em lista
         """
-        # BUG: Nunca vai retornar invertido, pois não foi inserido o .inverted()
-        return self.entries
+
+        return sorted(self.entries.keys(), reverse=True)
 
     def delete(self, name):
         """
@@ -114,3 +99,14 @@ class Phonebook:
         """
         self.entries.pop(name)
         return 'Numero deletado'
+
+    def change_number(self, name, number):
+        if name in self.entries:
+            self.entries[name] = number
+            return 'Numero alterado'
+        return 'Contato não encontrado'
+
+    def get_name_by_number(self, number):
+        for name, numero in self.entries.items():
+            if numero in number:
+                return name
--- test/test_phonebook.py
+++ test/test_phonebook.py
@@ -11,21 +11,28 @@ class TestPhonebook:
         assert resultado == resultado_esperado
 
 
-    def test_add_com_caracteres_especial_1(self):
+    def test_add_com_caracteres_especial_arroba(self):
         phonebook = Phonebook()
         resultado_esperado = "Nome invalido"
 
         resultado = phonebook.add("@", "11657897")
         assert resultado == resultado_esperado
 
-    def test_add_com_caracteres_especial_2(self):
+    def test_add_com_caracteres_especial_hashtag(self):
         phonebook = Phonebook()
         resultado_esperado = "Nome invalido"
 
         resultado = phonebook.add("#", "11657897")
         assert resultado == resultado_esperado
 
-    def test_add_com_caracteres_especial_3(self):
+    def test_add_com_caracteres_especial_exclamacao(self):
+        phonebook = Phonebook()
+        resultado_esperado = "Nome invalido"
+
+        resultado = phonebook.add("!", "11657897")
+        assert resultado == resultado_esperado
+
+    def test_add_com_caracteres_especial_cifrao(self):
         phonebook = Phonebook()
         resultado_esperado = "Nome invalido"
 
@@ -33,7 +40,7 @@ class TestPhonebook:
         assert resultado == resultado_esperado
 
 
-    def test_add_com_caracteres_especial_4(self):
+    def test_add_com_caracteres_especial_percentual(self):
         phonebook = Phonebook()
         resultado_esperado = "Nome invalido"
 
@@ -44,7 +51,125 @@ class TestPhonebook:
         phonebook = Phonebook()
         resultado_esperado = "Numero adicionado"
 
-        resultado = phonebook.add(" ", "11657897")
+        resultado = phonebook.add(" jose", " ")
         print(phonebook.get_phonebook_sorted())
         assert resultado == resultado_esperado
+
+    def test_lookup(self):
+        phonebook = Phonebook()
+        phonebook.add("Andre", "465465466")
+        resultado = "Andre", "465465466"
+        resultado_esperado = phonebook.lookup("Andre")
+        assert resultado == resultado_esperado
+        print(resultado_esperado)
+
+    def test_lookup_com_caracteres_especial_hashtag(self):
+        phonebook = Phonebook()
+        phonebook.add("#", "465465466")
+        resultado = "Nome invalido"
+        resultado_esperado = phonebook.lookup("#")
+        assert resultado == resultado_esperado
+        print(resultado_esperado)
+
+    def test_lookup_com_caracteres_especial_arroba(self):
+        phonebook = Phonebook()
+        phonebook.add("@", "465465466")
+        resultado = "Nome invalido"
+        resultado_esperado = phonebook.lookup("@")
+        assert resultado == resultado_esperado
+        print(resultado_esperado)
+
+    def test_lookup_com_caracteres_especial_exclamacao(self):
+        phonebook = Phonebook()
+        phonebook.add("ta!nah", "465465466")
+        resultado = "Nome invalido"
+        resultado_esperado = phonebook.lookup("ta!nah")
+        assert resultado == resultado_esperado
+        print(resultado_esperado)
+
+    def test_lookup_com_caracteres_especial_cifrao(self):
+        phonebook = Phonebook()
+        phonebook.add("$aulo", "465465466")
+        resultado = "Nome invalido"
+        resultado_esperado = phonebook.lookup("$aulo")
+        assert resultado == resultado_esperado
+        print(resultado_esperado)
+
+    def test_lookup_com_caracteres_especial_percentual(self):
+        phonebook = Phonebook()
+        phonebook.add("%", "465465466")
+        resultado = "Nome invalido"
+        resultado_esperado = phonebook.lookup("%")
+        assert resultado == resultado_esperado
+        print(resultado_esperado)
+
+    def test_get_names(self):
+        phonebook = Phonebook()
+        phonebook.add("Luis", "12345678")
+        resultado_esperado = {'POLICIA', 'Luis'}
+        resultado = phonebook.get_names()
+        assert resultado_esperado == resultado
+
+    def test_get_numbers(self):
+        phonebook = Phonebook()
+        phonebook.add("Luis", "12345678")
+        resultado_esperado = [ "190", "12345678"]
+        resultado = phonebook.get_numbers()
+        assert resultado_esperado == resultado
+
+    def test_clear(self):
+        phonebook = Phonebook()
+        resultado_esperado = 'phonebook limpado'
+        resultado = phonebook.clear()
+        assert resultado_esperado == resultado
+    def test_search(self):
+        phonebook = Phonebook()
+        phonebook.add("Andre", "123456")
+        phonebook.add("Andreia", "123")
+        phonebook.add("Andrea", "12345678")
+        resultado_esperado = [{'Andre', "123456"}, {'Andreia', '123'}, {'Andrea', "12345678"}]
+        resultado = phonebook.search("Andre")
+        assert resultado_esperado == resultado
+
+    def test_get_phonebook_sorted(self):
+        phonebook = Phonebook()
+        phonebook.add("Zebra", "000")
+        phonebook.add("Caio", "111")
+        resultado_esperado = ['Caio', 'POLICIA', 'Zebra']
+        resultado = phonebook.get_phonebook_sorted()
+        assert resultado == resultado_esperado
+
+    def test_get_phonebook_reverse(self):
+        phonebook = Phonebook()
+        phonebook.add("Zebra", "000")
+        phonebook.add("Caio", "111")
+        resultado_esperado = ['Zebra', 'POLICIA', 'Caio']
+        resultado = phonebook.get_phonebook_reverse()
+        assert resultado == resultado_esperado
+
+    def test_delete(self):
+        phonebook = Phonebook()
+        phonebook.add("Manoel", "333")
+        resultado = phonebook.delete('Manoel')
+        resultado_esperado = 'Numero deletado'
+        assert resultado == resultado_esperado
+
+    def test_change_number(self):
+        phonebook = Phonebook()
+        phonebook.add("Monica", "456")
+        resultado = phonebook.change_number('Monica', '789')
+        resultado_esperado = 'Numero alterado'
+        assert resultado == resultado_esperado
+
+    def test_get_name_by_number(self):
+        phonebook = Phonebook()
+        phonebook.add("Ze", "999")
+        resultado = phonebook.get_name_by_number('999')
+        resultado_esperado = 'Ze'
+        assert resultado == resultado_esperado
+
+
+
+
+
 
